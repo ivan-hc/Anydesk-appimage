f70123b43ac5453649da6cfeda83d796f13dae88
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-10-13 17:51:45 +0200

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -16,7 +16,7 @@ if test -f ./pkg2appimage; then
 	echo " pkg2appimage already exists" 1> /dev/null
 else
 	echo " Downloading pkg2appimage..."
-	wget -q https://raw.githubusercontent.com/ivan-hc/AM-application-manager/main/tools/pkg2appimage
+	wget -q https://github.com/AppImageCommunity/pkg2appimage/releases/download/continuous/pkg2appimage--x86_64.AppImage -O pkg2appimage
 fi
 chmod a+x ./appimagetool ./pkg2appimage
 
